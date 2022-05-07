b06978fa0b3b3fb65ae33880647a0ba4e24b6cd5
Nikola2412 <nikolamark50@gmail.com>
2022-05-07 13:49:51 +0200

    Test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -119,7 +119,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(199, 232);
+            this.button4.Location = new System.Drawing.Point(189, 232);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 8;
